e783f2d9ea9996ba1fc6166417c6805ff664b244
wrwr0az <ahasan0014@stu.kau.edu.sa>
2021-02-03 16:14:33 +0300

    final deoloyment front-end inside back-end

--- back-end/server.js
+++ back-end/server.js
@@ -123,7 +123,7 @@ app.use(express.static(path.join(__dirname, "build")));
 // After all routes
 // This code essentially serves the index.html file on any unknown routes.
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "front-end/build", "index.html"));
+  res.sendFile(path.join(__dirname, "build", "index.html"));
   console.log("dir name is: ", __dirname);
 });
 
