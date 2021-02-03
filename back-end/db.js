92bf083ff0e6759b68a75a7aa5ebc113671b111a
wrwr0az <ahasan0014@stu.kau.edu.sa>
2021-02-03 16:58:24 +0300

    final deoloyment front-end inside back-end

--- back-end/db.js
+++ back-end/db.js
@@ -13,7 +13,7 @@ const localDB = process.env.TESTENV ? database.test : database.development;
 
 // Environment variable MONGODB_URL will be available in
 // Heroku production environment, otherwise use Test or Development DB
-const currentDB = process.env.MONGODB_URL || localDB;
+const currentDB = process.env.MONGODB_URL;
 
 // Export the appropriate database based om the current environment
 module.exports = currentDB;
