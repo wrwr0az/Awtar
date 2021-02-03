68ecab2b1937fdeaddce099e01593e1b518ce480
wrwr0az <ahasan0014@stu.kau.edu.sa>
2021-02-03 15:44:38 +0300

    final deoloyment front-end inside back-end

--- back-end/front-end/src/apiConfig.js
+++ back-end/front-end/src/apiConfig.js
@@ -2,11 +2,11 @@ const EXPRESS_PORT = 5000;
 
 const apiUrls = {
   development: `http://localhost:${EXPRESS_PORT}`,
-  production: `https://YOUR_HEROKU_URL.herokuapp.com`,
+  production: `https://awtar.herokuapp.com`,
 };
 
 const API_URL =
-  window.location.hostname === 'localhost'
+  window.location.hostname === "localhost"
     ? apiUrls.development
     : apiUrls.production;
 
